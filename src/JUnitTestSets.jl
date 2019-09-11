ca81610dd926a949bbc6d81e3139a407c7837c96
Matt Koski <maccam912@gmail.com>
2019-09-11 11:12:24 -0500

    initial commit

--- src/JUnitTestSets.jl
+++ src/JUnitTestSets.jl
@@ -1,5 +1,23 @@
 module JUnitTestSets
 
-greet() = print("Hello World!")
+using Test, LightXML
+
+mutable struct JUnitTestSet <: Test.AbstractTestSet
+    id::String
+    name::String
+    tests::Int
+    failures::Int
+    time::Float64
+    results::Vector{Union{JUnitTestSet, Test.Result}}
+    JUnitTestSet(desc="JUnit Test Set") = new("1", desc, 0, 0, 0, [])
+end
+Test.record(ts::Testsuite, child) = push!(ts.results, child)
+
+function Test.finish(ts::Testsuite)
+    if Test.get_testset_depth() > 0
+        Test.record(Test.get_testset(), ts)
+    end
+    ts
+end
 
 end # module
